92b7b4537d5665562aaebae14fd0228b01912247
ZAINAB TAREEN <rl190058@iphone-59.rlas-116.org>
2018-05-17 09:56:54 -0500

    baseball in an array

--- BaseballApp/ViewController.swift
+++ BaseballApp/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var basball: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+       
 
-    override func didReceiveMemoryWarning() {
+        func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
 
 
 }
 
+}
